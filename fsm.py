6710a7116db20deea1243d2fe07dafd9e05f8b50
Stevão Andrade <stevao.andrade@gmail.com>
2015-11-20 16:50:42 -0200

    Create the basic structure to the project

---
+++ fsm.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+	This class will represent a FiniteStateMachine
+"""
+class FiniteStateMachine(self):
+
+	"""docstring for FiniteStateMachine"""
+	
+	#Contructor
+	def __init__(self, arg):
+		super(FiniteStateMachine, self).__init__()
+		self.arg = arg
---
+++ util.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fsm import FiniteStateMachine
+
+
+#This script will handle the file operations
+
+
+"""
+	Read a fsm from a file and returns a FiniteStateMachine obj
+"""
+def read_fsm(txt):
+	
+	fsm = FiniteStateMachine()
+
+	return fsm
+
+
+
+"""
+	Read the sequences from a file and return into a list to run it into a fsm
+"""
+def read_sequences(txt):
+ 	
+ 	sequence = []
+
+ 	return sequence 
